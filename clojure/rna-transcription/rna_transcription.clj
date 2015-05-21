808d9ff5f5d3bf83748c0a568b6fc6d4df1a1046
pnaranja <paul.naranja@gmail.com>
2015-05-21 00:43:59 -0700

    Cleaner looking lambda function for clojure.string/replace

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -3,4 +3,4 @@
 
 (defn to-rna [dna]
   {:pre [(re-matches #"[GCTA]*" dna)]}
-  (clojure.string/replace dna #"C|G|A|T" {"C" "G", "G" "C", "A" "U", "T" "A"}))
+  (clojure.string/replace dna #"[CGAT]" {"C" "G", "G" "C", "A" "U", "T" "A"}))
