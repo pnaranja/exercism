b21c31eec24f3f76e9d8f1584dbc56cc76d71a67
pnaranja <paul.naranja@gmail.com>
2015-05-21 01:32:42 -0700

    added dna-rna mapping

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -1,7 +1,9 @@
 (ns rna-transcription
-  (:require [clojure.string :as str :refer [replace]] ))
+  (:require [clojure.string :as str :refer [replace]]))
 ;C->G, G->C, A->U, T->A
+
+(defn- dna-rna {"C" "G", "G" "C", "A" "U", "T" "A"})
 
 (defn to-rna [dna]
   {:pre [(re-matches #"[GCTA]*" dna)]}
-  (str/replace dna #"[CGAT]" {"C" "G", "G" "C", "A" "U", "T" "A"}))
+  (str/replace dna #"[CGAT]" dna-rna))
