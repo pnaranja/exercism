cb08612b49b2944323cbea89f0db629c6234a85c
pnaranja <paul.naranja@gmail.com>
2015-05-23 12:53:59 -0700

    function anagram? is working

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,3 +1,11 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str :refer [lower-case split]])
+  )
+
+(defn anagram? [W1 W2] 
+  (let [w1 (str/lower-case W1)
+        w2 (str/lower-case W2)]
+    (and (not (= w1 w2))
+         (= (sort (str/split w1 #"")) (sort (str/split w2 #""))))))
 
 (defn anagrams-for [word word-list])
