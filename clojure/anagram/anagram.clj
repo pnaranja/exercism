7a9f13602650e0695a3fbd65af0f13357c6a76e2
pnaranja <paul.naranja@gmail.com>
2015-05-23 19:32:38 -0700

    Did not need to change into [] for the tests to pass.  Also forgot Clojure treats strings as seq of characters.

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,10 +1,10 @@
 (ns anagram
   (:require [clojure.string :as str :refer [lower-case split]]))
 
-(defn anagram? [W1 W2] 
+(defn- anagram? [W1 W2] 
   (let [w1 (str/lower-case W1)
         w2 (str/lower-case W2)]
     (and (not (= w1 w2))
-         (= (sort (str/split w1 #"")) (sort (str/split w2 #""))))))
+         (= (sort w1) (sort w2)))))
 
-(defn anagrams-for [word word-list] (into [] (filter #(anagram? word %) word-list)))
+(defn anagrams-for [word word-list]  (filter #(anagram? word %) word-list))
