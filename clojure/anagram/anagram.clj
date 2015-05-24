d660e2d501bd7d33489431a6ff1d5c62bc2add3a
pnaranja <paul.naranja@gmail.com>
2015-05-23 18:28:36 -0700

    Added filter and into [] to finish code

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -1,6 +1,5 @@
 (ns anagram
-  (:require [clojure.string :as str :refer [lower-case split]])
-  )
+  (:require [clojure.string :as str :refer [lower-case split]]))
 
 (defn anagram? [W1 W2] 
   (let [w1 (str/lower-case W1)
@@ -8,4 +7,4 @@
     (and (not (= w1 w2))
          (= (sort (str/split w1 #"")) (sort (str/split w2 #""))))))
 
-(defn anagrams-for [word word-list])
+(defn anagrams-for [word word-list] (into [] (filter #(anagram? word %) word-list)))
