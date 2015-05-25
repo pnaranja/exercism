ea5b0ceab1f67be29ab12a1d9fe9468b0a0d1d3c
pnaranja <paul.naranja@gmail.com>
2015-05-25 00:27:48 -0700

    Finished verse function

--- clojure/beer-song/beer_song.clj
+++ clojure/beer-song/beer_song.clj
@@ -1,3 +1,17 @@
 (ns beer-song)
 
-(defn verse [str-beers end-beers])
+;[?] of beer on the wall, [?] of beer.
+;[?] , [?] of beer on the wall.
+
+(defn song_template [beers1 beers2 take_or_buy beers_on_wall]
+  (str beers1 " of beer on the wall, " beers2 " of beer.\n" take_or_buy ", " beers_on_wall " of beer on the wall.\n"))
+
+(defn verse [beers]
+  (case (int beers) 
+    2 (song_template "2 bottles" "2 bottles" "Take one down and pass it around" "1 bottle")
+    1 (song_template "1 bottle" "1 bottle" "Take it down and pass it around" "no more bottles")
+    0 (song_template "No more bottles" "no more bottles" "Go to the store and buy some more" "99 bottles") 
+    (song_template  (str beers " bottles") (str beers " bottles") "Take one down and pass it around" (str (- beers 1) " bottles"))))
+
+
+(defn song [start-beers end-beers])
