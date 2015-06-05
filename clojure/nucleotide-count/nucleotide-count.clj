14d8864b6940c02d3c2835731e96d3ab979bbf25
pnaranja <paul.naranja@gmail.com>
2015-06-04 23:42:53 -0700

    First attempt for count function

--- clojure/nucleotide-count/nucleotide-count.clj
+++ clojure/nucleotide-count/nucleotide-count.clj
@@ -1,6 +1,7 @@
 (ns nucleotide-count)
 
-(defn count [nucleotide dna])
+(defn count [nucleotide dna]
+  ((nucleotide-counts dna) nucleotide)
 
 (defn nucleotide-counts [dna]
   (frequencies  (seq (char-array dna)))
