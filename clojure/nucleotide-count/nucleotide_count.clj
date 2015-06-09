249a438d303cbe60df4b7e44cc1e9f63fe2564c0
pnaranja <paul.naranja@gmail.com>
2015-06-09 00:59:03 -0700

    Add function to add unused nucleotides

--- clojure/nucleotide-count/nucleotide_count.clj
+++ clojure/nucleotide-count/nucleotide_count.clj
@@ -1,13 +1,13 @@
 (ns nucleotide-count)
 
+(defn add-unused-dna [counts]
+  (merge-with + counts {\A 0, \T 0, \C 0, \G 0}))
+
 (defn nucleotide-counts [dna]
-  (if (empty? dna)
-    {\A 0, \T 0, \C 0, \G 0}
-   (frequencies (seq (char-array dna)))))
+  (add-unused-dna (frequencies (seq (char-array dna)))))
 
 (defn count [nucleotide dna]
   (if (empty? dna) 
     0 
-    ((nucleotide-counts dna) nucleotide)
-    )
+    ((nucleotide-counts dna) nucleotide))
   )
