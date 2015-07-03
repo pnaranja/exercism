58d1fffa9dfc0e7cd4bfaf396d6408c71cfa51fc
pnaranja <paul.naranja@gmail.com>
2015-07-03 16:39:36 -0700

    Improved nucleotide_count Clojure exercise

--- clojure/nucleotide-count/nucleotide_count.clj
+++ clojure/nucleotide-count/nucleotide_count.clj
@@ -4,9 +4,6 @@
   (merge {\A 0, \T 0, \C 0, \G 0} (frequencies dna)))
 
 (defn count [nucleotide dna]
-  (if (contains? #{\A \T \C \G} nucleotide) 
-    (if (empty? dna) 
-      0 
-      ((nucleotide-counts dna) nucleotide))
-    (throw (Exception. "invalid nucleotide")))
+    (or ((nucleotide-counts dna) nucleotide)
+        (throw (Exception. "invalid nucleotide")))
 )
