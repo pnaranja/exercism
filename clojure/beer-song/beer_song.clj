003af803889b6632ee2c7ee818c39e92ea047b15
pnaranja <paul.naranja@gmail.com>
2015-09-23 02:52:41 -0700

    Wrote out full strings instead of trying to build from a template -> More readable

--- clojure/beer-song/beer_song.clj
+++ clojure/beer-song/beer_song.clj
@@ -1,17 +1,12 @@
 (ns beer-song)
 
-;[?] of beer on the wall, [?] of beer.
-;[?] , [?] of beer on the wall.
-
-(defn- song_template [beers1 beers2 take_or_buy beers_on_wall]
-  (str beers1 " of beer on the wall, " beers2 " of beer.\n" take_or_buy ", " beers_on_wall " of beer on the wall.\n"))
-
 (defn verse [beers]
   (case (int beers) 
-    2 (song_template "2 bottles" "2 bottles" "Take one down and pass it around" "1 bottle")
-    1 (song_template "1 bottle" "1 bottle" "Take it down and pass it around" "no more bottles")
-    0 (song_template "No more bottles" "no more bottles" "Go to the store and buy some more" "99 bottles") 
-    (song_template  (str beers " bottles") (str beers " bottles") "Take one down and pass it around" (str (- beers 1) " bottles"))))
+    2 (str "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
+    1 (str "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
+    0 (str "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n") 
+    (str beers " bottles of beer on the wall, " beers " bottles of beer.\n" "Take one down and pass it around, " 
+         (str (- beers 1)  " bottles of beer on the wall.\n"))))
 
 (defn- apply_verses [start-beers end-beers]
   (clojure.string/join "\n" (map (fn [x] (verse x)) (range start-beers (- end-beers 1) -1))))
